7d1f0fb67df0e3b3fea27b17901f79483ada5f2e
"Dheeraj Remix" <"Dheeraj160199@gmail.com">
2022-07-18 23:19:25 +0530

    Library and chainLink in solidity

---
+++ contracts/fundme.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe{
+
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 50 * 1e18;    // 1 * 10 ** 18
+
+    function fund() public payable{
+
+        msg.value.getConversionRate();
+        // require((msg.value) >= minimumUsd,"Didn't send enough !"); 
+
+    }
+
+    
+
+
+}
