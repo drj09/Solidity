ea6d14d62f1fd9a0cf0313c7b93cf68be9436132
"Dheeraj Remix" <"Dheeraj160199@gmail.com">
2022-07-19 17:32:45 +0530

    Sending ETH from a Contract

--- contracts/fundme.sol
+++ contracts/fundme.sol
@@ -19,7 +19,16 @@ contract FundMe{
         addressToAmountFunded[msg.sender] += msg.value; 
     }
 
-    function withdraw() public {
+    address public owner;
+    constructor(){
+        // we are using owner variable to define who is the owner of this contract and then only owner will be able to withdraw the amount
+        owner = msg.sender;
+    }
+    
+    
+    
+    
+    function withdraw() public onlyOwner{
 
         for(uint256 funderIndex = 0;funderIndex < funders.length; funderIndex = funderIndex++){
             //code
@@ -29,19 +38,19 @@ contract FundMe{
         //reset the array
         funders = new address[](0);
         //actually withdraw funds
-
-
         /*there are 3 ways to send Ether:
             1. transfer  :  throw error   and gas fees is 2300
             2. send      :  give boolean if transfer was successful   and gas fees is 2300 
             3. call      :  can Modifiy gas fees here
         */
         payable(msg.sender).transfer(address(this).balance);
-
-
-
     }
-
-
+ 
+    //modifier : Function Modifiers are used to modify the behaviour of a function.
+    modifier onlyOwner {
+        //_;   // if you want to run function then modifier 
+        require(msg.sender == owner,"Only owner can withdraw");
+        _;  //do rest of the code  first the modifier will be called then the other function will be invoked.
+    }
 
 }
